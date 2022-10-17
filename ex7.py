159841292e51366e509b176ad5d4afd0067ef7e2
Leahu Vlad Iulius <leahu.vlad.iulius@gmail.com>
2022-10-17 12:33:28 +0300

    Small change

--- ex7.py
+++ ex7.py
@@ -1,10 +1,6 @@
 # Ex 7
 def palindrome_tuple(numbers):
-    def is_palindrome(number):
-        string = str(number)
-        return string == string[::-1]
-
-    ret_lst = [number for number in numbers if is_palindrome(number)]
+    ret_lst = [number for number in numbers if str(number) == str(number)[::-1]]
 
     return (len(ret_lst), max(ret_lst))
 
